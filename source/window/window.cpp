c447a8101aa3402a9521901aaf579469cc401297
knz13 <82895172+knz13@users.noreply.github.com>
2022-04-14 23:20:15 -0300

    Adding depth testing and face culling

--- source/window/window.cpp
+++ source/window/window.cpp
@@ -80,6 +80,8 @@ Window::Window(WindowCreationProperties prop) : m_Properties(prop) {
     
     GL_CALL(glEnable(GL_PROGRAM_POINT_SIZE));
     GL_CALL(glEnable(GL_STENCIL_TEST));
+    GL_CALL(glEnable(GL_DEPTH_TEST));
+    GL_CALL(glEnable(GL_CULL_FACE));
 
     glfwSetKeyCallback(this->GetContextPointer(),[](GLFWwindow* ptr,int key, int scancode, int action, int mods){
         Window& win = *Window::GetWindow(ptr);
